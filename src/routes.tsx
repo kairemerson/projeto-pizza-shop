73a6598fda910e1257b90389dbb3c032e7c21de2
Remerson Monteiro <kairemerson@gmail.com>
2025-05-06 13:56:38 -0300

    chore: add notFound to routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Dashboard } from "./pages/app/Dashboard";
+import { Dashboard } from "./pages/app/Dashboard/Dashboard";
 import { Signin } from "./pages/auth/Signin";
 import { AppLayout } from "./_layouts/app";
 import { AuthLayout } from "./_layouts/auth";
 import { Signup } from "./pages/auth/Signup";
+import { Orders } from "./pages/app/Orders/Orders";
+import { NotFound } from "./pages/404";
 
 export const router = createBrowserRouter([
     {
         path: "/", 
         element: <AppLayout/>,
-        children: [{path: "/", element: <Dashboard/>}]
+        errorElement: <NotFound/>,
+        children: [
+            {path: "/", element: <Dashboard/>},
+            {path: "/orders", element: <Orders/>}
+        ]
     },
     {
         path: "/", 
