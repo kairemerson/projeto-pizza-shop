f4fe7b2966542a54a50802099f3aed920c5383ac
Remerson Monteiro <kairemerson@gmail.com>
2025-05-09 11:12:10 -0300

    feat: created component Button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -11,6 +11,7 @@ const buttonVariants = cva(
       variant: {
         default: 'bg-primary text-primary-foreground hover:bg-primary/90',
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+        success: 'bg-emerald-500 text-white hover:bg-emerald-500/90 dark:bg-emerald-600 dark:hover:bg-emerald-600/80',
         outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
@@ -19,6 +20,7 @@ const buttonVariants = cva(
       size: {
         default: 'h-10 px-4 py-2',
         sm: 'h-9 px-3 rounded-md',
+        xs: "h-8 px-2.5",
         lg: 'h-11 px-8 rounded-md',
         icon: 'h-10 w-10',
       },
