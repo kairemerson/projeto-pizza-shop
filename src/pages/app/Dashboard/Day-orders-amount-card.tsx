ca5b342f6549a905b9d6ef7c03d49eb19958988e
Remerson Monteiro <kairemerson@gmail.com>
2025-05-09 11:10:45 -0300

    feat: created functions to dashboard

---
+++ src/pages/app/Dashboard/Day-orders-amount-card.tsx
@@ -0,0 +1,25 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Utensils } from "lucide-react";
+
+
+export function DayOrdersAmountCard() {
+    return (
+        <Card>
+            <CardHeader className="flex items-center justify-between space-y-0 pb-2">
+                <CardTitle className="font-semibold">Pedidos (mês)</CardTitle>
+                <Utensils className="h-4 w-4 text-muted-foreground"/>
+            </CardHeader>
+            <CardContent className="space-y-1">
+                <span className="text-2xl font-bold tracking-tight">
+                    10
+                </span>
+                <p className="text-xs text-muted-foreground mt-1">
+                    <span className="text-rose-500 dark:text-rose-400" >
+                        -4%
+                    </span>{" "}
+                    em relação a ontem
+                </p>
+            </CardContent>
+        </Card>
+    )
+}
